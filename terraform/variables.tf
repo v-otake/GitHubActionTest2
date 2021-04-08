2d4119d91f4af07d2cab00a13a30b63c0043fa6d
v-otake <81287446+v-otake@users.noreply.github.com>
2021-04-08 17:55:25 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "GitHubActionTest"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "otake-githubactiontest"
 }
 
 variable "location" {
